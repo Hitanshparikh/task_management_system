965b47da5d021e479e5bbeca9aaf672f202b6368
Hitansh Parikh <hitanshpparikh@gmail.com>
2024-09-20 01:03:53 +0530

    Add files via upload

--- database/tms_db.sql
+++ database/tms_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 17, 2024 at 08:24 PM
+-- Generation Time: Sep 19, 2024 at 04:34 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -44,13 +44,19 @@ CREATE TABLE `project_list` (
 --
 
 INSERT INTO `project_list` (`id`, `name`, `description`, `status`, `start_date`, `end_date`, `manager_id`, `user_ids`, `date_created`) VALUES
-(3, 'Vaibhavi Patel', 'All the Tasks for Vaibhavi Patel are here', 0, '2024-01-01', '2025-12-01', 13, '6', '2024-09-17 09:27:35'),
-(4, 'Shrusti gajjar', '						All the tasks for Shrusti Gajjar are here.					', 0, '2024-01-01', '2025-12-01', 13, '7', '2024-09-17 09:32:21'),
-(5, 'Abhishek Patel', '						All the tasks for Abhishek Patel are here.					', 0, '2024-01-01', '2025-12-01', 13, '9', '2024-09-17 09:33:57'),
-(6, 'Pinal Hansora', '						All the tasks for Pinal Hansora are here.					', 0, '2024-01-01', '2025-12-01', 13, '10', '2024-09-17 09:38:44'),
-(7, 'Avani Khokharia', 'All the tasks for Avani Khokharia are here.', 0, '2024-01-01', '2025-12-01', 13, '8', '2024-09-17 09:39:50'),
-(8, 'Jigar Sarda', 'All the tasks for Jigar Sarda are here.', 0, '2024-01-01', '2025-12-01', 13, '12', '2024-09-17 09:40:47'),
-(9, 'Harshul Yagnik', '						All the tasks for Harshul Yagnik are here.					', 0, '2024-01-11', '2025-12-01', 13, '11', '2024-09-17 09:42:43');
+(1, 'Amit Thakkar', 'Admin', 0, '2024-01-01', '2025-12-31', 2, '1', '2024-09-18 12:50:23'),
+(2, 'Project Manager', 'Add or assign tasks and check progress.', 0, '2024-01-01', '2025-12-31', 2, '2', '2024-09-18 12:53:22'),
+(3, 'Vaibhavi Patel', 'All the Tasks for Vaibhavi Patel are here', 0, '2024-01-01', '2025-12-01', 2, '3', '2024-09-17 09:27:35'),
+(4, 'Shrusti gajjar', '						All the tasks for Shrusti Gajjar are here.					', 0, '2024-01-01', '2025-12-01', 2, '4', '2024-09-17 09:32:21'),
+(5, 'Abhishek Patel', '						All the tasks for Abhishek Patel are here.					', 0, '2024-01-01', '2025-12-01', 2, '5', '2024-09-17 09:33:57'),
+(6, 'Pinal Hansora', '						All the tasks for Pinal Hansora are here.					', 0, '2024-01-01', '2025-12-01', 2, '6', '2024-09-17 09:38:44'),
+(7, 'Avani Khokharia', 'All the tasks for Avani Khokharia are here.', 0, '2024-01-01', '2025-12-01', 2, '7', '2024-09-17 09:39:50'),
+(8, 'Jigar Sarda', 'All the tasks for Jigar Sarda are here.', 0, '2024-01-01', '2025-12-01', 2, '8', '2024-09-17 09:40:47'),
+(9, 'Harshul Yagnik', '						All the tasks for Harshul Yagnik are here.					', 0, '2024-01-11', '2025-12-01', 2, '9', '2024-09-17 09:42:43'),
+(10, 'Hemang Thakar', '&lt;font color=&quot;#000000&quot; face=&quot;sans-serif&quot;&gt;&lt;span style=&quot;font-size: 13.12px; white-space-collapse: preserve;&quot;&gt;All the tasks for Hemang Thakar are here.&lt;/span&gt;&lt;/font&gt;											', 0, '2024-01-01', '2025-12-31', 2, '10', '2024-09-18 13:08:19'),
+(11, 'Vidisha Pradhan', '&lt;font color=&quot;#444444&quot; face=&quot;sans-serif&quot;&gt;&lt;span style=&quot;font-size: 13.12px; white-space-collapse: preserve;&quot;&gt;All the tasks for Vidisha Pradhan are here.&lt;/span&gt;&lt;/font&gt;											', 0, '2024-01-01', '2025-12-31', 2, '11', '2024-09-18 13:09:16'),
+(12, 'Dhara Solanki', 'All the tasks for Dhara Solanki are here.											', 0, '2024-01-01', '2025-12-31', 2, '12', '2024-09-18 13:10:11'),
+(13, 'Akshita Kadam', '&lt;div&gt;All the tasks for Akshita&lt;/div&gt;&lt;div&gt;Kadam are here.&lt;/div&gt;											', 0, '2024-01-01', '2025-12-31', 2, '13', '2024-09-18 13:10:47');
 
 -- --------------------------------------------------------
 
@@ -98,7 +104,8 @@ INSERT INTO `task_list` (`id`, `project_id`, `task`, `description`, `status`, `d
 (6, 5, 'test 1', '							', 1, '2024-09-17 09:51:25'),
 (7, 7, 'test 2', '							', 1, '2024-09-17 09:52:56'),
 (8, 9, 'test 3', '							', 1, '2024-09-17 09:53:11'),
-(9, 8, 'test 4', '							', 1, '2024-09-17 09:53:25');
+(9, 8, 'test 4', '							', 1, '2024-09-17 09:53:25'),
+(10, 5, 'submit papers', 'papers to be submitted by tomorrow', 1, '2024-09-18 10:23:11');
 
 -- --------------------------------------------------------
 
@@ -122,18 +129,19 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `password`, `type`, `avatar`, `date_created`) VALUES
-(1, 'Amit', 'Thakkar', 'amit@hod', '17d84f171d54c301fabae1391a125c4e', 1, '1726477200_images.jpeg', '2020-11-26 10:57:04'),
-(6, 'Vaibhavi', 'Patel', 'vaibhavipatel@charusat.in', '08f41e2b56730d87f1232d525303ba14', 3, '1726551000_VaibhaviPatel.webp', '2024-09-17 09:17:34'),
-(7, 'Srushti', 'Gajjar', 'srushti@gajjar.charusat.in', '5dae429688af1c521ad87ac394192c6d', 3, '1726551060_SrushtiGajjar.webp', '2024-09-17 09:18:35'),
-(8, 'avani', 'khokharia', 'avani@khokharia.charusat.in', '17540aef7b8470cc3ea8b2b9046af3b6', 3, '1726548120_download (2).jpeg', '2024-09-17 09:19:25'),
-(9, 'Abhishek', 'patel', 'Abhishek@patel.charusat.in', '62c428533830d84fd8bc77bf402512fc', 3, '1726548060_download (1).jpeg', '2024-09-17 09:20:05'),
-(10, 'Pinal', 'Hansora', 'pinal@hansora.charusat.in', 'da984e42a5899bbdac496ef0cbadcee2', 3, '1726550640_1725344474065.jpeg', '2024-09-17 09:21:01'),
-(11, 'harshul', 'yagnik', 'harshul@yagnik.charusat.in', '035ed2311b96d2a65ec6a6fe71046c14', 3, '1726548000_download.jpeg', '2024-09-17 09:21:55'),
-(12, 'Jigar', 'Sarda', 'Jigar@Sarda.charusat.in', '32981a13284db7a021131df49e6cd203', 3, '1726550640_images (1).jpeg', '2024-09-17 09:22:56'),
-(13, 'Task', 'Manager', 'manager@cse', 'd6fd0924e324f50669ae0295adf59567', 2, '1726550700_360_F_227450952_KQCMShHPOPebUXklULsKsROk5AvN6H1H.jpg', '2024-09-17 09:26:28'),
-(14, 'Hemang', 'Thakar', 'hemang@thakar.charusat.in', 'eb5e48e74123cacc52761302ea4a7d22', 3, '1726551000_HemangThakar.webp', '2024-09-17 11:00:36'),
-(15, 'Vidisha', 'Pradhan', 'vidisha@pradhan.charusat.in', '08f41e2b56730d87f1232d525303ba14', 3, '1726551120_VidishaPradhan.webp', '2024-09-17 11:02:32'),
-(16, 'Dhara', 'Solanki', 'dhara@solanki.charusat.in', '522748524ad010358705b6852b81be4c', 3, '1726551180_DharaSolanki.webp', '2024-09-17 11:03:35');
+(1, 'Amit', 'Thakkar', 'amitthakkar.it@charusat.ac.in', '17d84f171d54c301fabae1391a125c4e', 1, '1726477200_images.jpeg', '2020-11-26 10:57:04'),
+(2, 'Task', 'Manager', 'manager@cse', 'd6fd0924e324f50669ae0295adf59567', 2, '1726550700_360_F_227450952_KQCMShHPOPebUXklULsKsROk5AvN6H1H.jpg', '2024-09-17 09:26:28'),
+(3, 'Vaibhavi', 'Patel', 'vaibhavipatel.cse@charusat.ac.in', '08f41e2b56730d87f1232d525303ba14', 3, '1726551000_VaibhaviPatel.webp', '2024-09-17 09:17:34'),
+(4, 'Srushti', 'Gajjar', 'srushtigajjar.cse@charusat.ac.in', '5dae429688af1c521ad87ac394192c6d', 3, '1726551060_SrushtiGajjar.webp', '2024-09-17 09:18:35'),
+(5, 'Abhishek', 'patel', 'abhishekpatel.cse@charusat.ac.in', '62c428533830d84fd8bc77bf402512fc', 3, '1726548060_download (1).jpeg', '2024-09-17 09:20:05'),
+(6, 'Pinal', 'Hansora', 'pinalhansora.cse@charusat.ac.in\r\n', 'da984e42a5899bbdac496ef0cbadcee2', 3, '1726550640_1725344474065.jpeg', '2024-09-17 09:21:01'),
+(7, 'avani', 'khokharia', 'avanikhokhariya.cse@charusat.ac.in', '17540aef7b8470cc3ea8b2b9046af3b6', 3, '1726548120_download (2).jpeg', '2024-09-17 09:19:25'),
+(8, 'Jigar', 'Sarda', 'jigarsarda.ee@charusat.ac.in', '32981a13284db7a021131df49e6cd203', 3, '1726550640_images (1).jpeg', '2024-09-17 09:22:56'),
+(9, 'harshul', 'yagnik', 'harshulyagnik.cse@charusat.ac.in\r\n', '035ed2311b96d2a65ec6a6fe71046c14', 3, '1726548000_download.jpeg', '2024-09-17 09:21:55'),
+(10, 'Hemang', 'Thakar', 'hemangthakar.cse@charusat.ac.in', 'eb5e48e74123cacc52761302ea4a7d22', 3, '1726551000_HemangThakar.webp', '2024-09-17 11:00:36'),
+(11, 'Vidisha', 'Pradhan', 'vidishapradhan.cse@charusat.ac', '08f41e2b56730d87f1232d525303ba14', 3, '1726551120_VidishaPradhan.webp', '2024-09-17 11:02:32'),
+(12, 'Dhara', 'Solanki', 'dharasolanki.cse@charusat.ac.in', '522748524ad010358705b6852b81be4c', 3, '1726551180_DharaSolanki.webp', '2024-09-17 11:03:35'),
+(13, 'Akshita', 'Kadam', 'akshitakadam.cse@charusat.ac.in', '17540aef7b8470cc3ea8b2b9046af3b6', 3, '1726642320_Akshitakadam.webp', '2024-09-18 12:22:24');
 
 -- --------------------------------------------------------
 
@@ -154,6 +162,13 @@ CREATE TABLE `user_productivity` (
   `time_rendered` float NOT NULL,
   `date_created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_productivity`
+--
+
+INSERT INTO `user_productivity` (`id`, `project_id`, `task_id`, `comment`, `subject`, `date`, `start_time`, `end_time`, `user_id`, `time_rendered`, `date_created`) VALUES
+(5, 3, 5, 'started working on it', 'started working', '2024-09-18', '10:33:00', '14:33:00', 6, 4, '2024-09-18 10:34:02');
 
 --
 -- Indexes for dumped tables
@@ -197,7 +212,7 @@ ALTER TABLE `user_productivity`
 -- AUTO_INCREMENT for table `project_list`
 --
 ALTER TABLE `project_list`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `system_settings`
@@ -209,19 +224,19 @@ ALTER TABLE `system_settings`
 -- AUTO_INCREMENT for table `task_list`
 --
 ALTER TABLE `task_list`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `user_productivity`
 --
 ALTER TABLE `user_productivity`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
